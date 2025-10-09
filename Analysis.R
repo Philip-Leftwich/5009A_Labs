2c61d483fd9006632e15508346dba70f9f2eaa6c
Philip Leftwich <philip.leftwich@gmail.com>
2025-10-09 13:41:54 +0000

    New 2025 commits

--- Analysis.R
+++ Analysis.R
@@ -25,6 +25,26 @@ LData_long %>%
   labs(x = "Bacterial Strain + plasmid pRB318",
        y = "Log10 Colony Forming Units+1")
 
+####
+
+LData_long %>% 
+  ggplot(aes(x = name,
+             y = value+1))+
+  geom_boxplot(width = .1,
+               position = position_nudge(x= .2),
+               outlier.shape = NA,
+               fill = "lightblue")+
+  geom_half_violin(position = position_nudge(x= -.2),
+                   fill = "lightblue")+
+  geom_beeswarm(shape = 21,
+                fill = "lightblue",
+                colour = "black",
+                cex = .5)+
+  scale_y_log10(labels = label_log()) +
+  theme_minimal()+
+  labs(x = "Bacterial Strain + plasmid pRB318",
+       y = "Log10 Colony Forming Units+1")
+
 # Model====
 
 lsmodel1 <- lm(value+1 ~ name, data = LData_long)
